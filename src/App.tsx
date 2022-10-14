1e1363f029073ff0465aada798e89c6212fec534
Rogerio Tadim <rogerio.tadim@dataprev.gov.br>
2022-10-14 01:30:24 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <>
+
+      <h1 className="text-2xl">Vite + React</h1>
+      <button className="bg-cian-300 hover:bg-cian-500 px-4 py-2 rounded text-md text-gray-900"> botão </button>
+
+    </>
+  )
+}
